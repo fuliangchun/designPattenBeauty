767fda8f60bfd1b11064210330c4c86b0e78dfcc
fuliangchun <FULIANGCHUN288@pingan.com.cn>
2020-12-23 22:49:14 +0800

    firstcommit

---
+++ src/main/java/com/geek/designbeauty/AppliactionContext.java
@@ -0,0 +1,17 @@
+package com.geek.designbeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author FULIANGCHUN288
+ * @description
+ * @date 2020年12月23日 22:38
+ * @since 1.0.0
+ **/
+@SpringBootApplication
+public class AppliactionContext {
+    public static void main(String[] args) {
+        SpringApplication.run(AppliactionContext.class, args);
+    }
+}
